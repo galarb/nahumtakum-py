5da83d03bb75eca2d01f329db8b6ec76ce51b313
gal arbel <84223483+galarb@users.noreply.github.com>
2025-01-26 10:39:09 +0200

    Update nahumtakumrobot.py

--- nahumtakumrobot.py
+++ nahumtakumrobot.py
@@ -8,8 +8,8 @@ i2c = I2C(1, scl=Pin(16), sda=Pin(17))  # running out of pins...
 # Create NahumTakum instance
 
 robot = NahumTakum(
-    in1=0,
-    in2=4,
+    in1=4,
+    in2=0,
     ena=13,
     encoder1_pin=14,
     encoder2_pin=12,
@@ -20,11 +20,21 @@ robot = NahumTakum(
 # Initialize the MPU6050 and system
 robot.begin()
 
-robot.gotoang(90)
-for _ in range(1000):
-    robot.run()
-    robot.tumble(1,0.01,0)
-    robot.stop()
+#robot.motgo(-90)
+#time.sleep(2)
+#robot.stop()
+#robot.testtumble(kp,ki,kd,lineindex)
+robot.gotoang(-90)
+
+robot.testtumble(1, 2, 0, 1)
+time.sleep(2)
+
+'''robot.gotoang(-90)
+robot.testtumble(2, 0, 0.1, 2)
+time.sleep(2)'''
+robot.stop()
+
+
 # Main loop
 #angle_y = robot.mpu.update_angle()
     
